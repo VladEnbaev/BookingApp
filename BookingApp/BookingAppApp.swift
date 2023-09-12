1e49ec4bf598032eb8962de17f0c0884f234f61f
VladGradskiy <enbaev.vlad4@gmail.com>
2023-09-12 17:33:35 +0300

    Initial Commit

---
+++ BookingApp/BookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppApp.swift
+//  BookingApp
+//
+//  Created by Влад Енбаев on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
