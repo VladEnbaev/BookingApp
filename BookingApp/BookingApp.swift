8fbbd5ee77177be92fe76fb164c7b8a41db16ade
VladGradskiy <enbaev.vlad4@gmail.com>
2023-09-12 17:51:37 +0300

    setup project

--- BookingApp/BookingApp.swift
+++ BookingApp/BookingApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct BookingAppApp: App {
+struct BookingApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ BookingApp/HotelScreen/HotelScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  HotelScreenView.swift
+//  BookingApp
+//
+//  Created by Влад Енбаев on 12.09.2023.
+//
+
+import SwiftUI
+
+struct HotelScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HotelScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        HotelScreenView()
+    }
+}
